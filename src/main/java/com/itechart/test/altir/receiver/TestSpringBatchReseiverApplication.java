9d0db28d23dbd9a45e253ec03fda4b1f8f5398fe
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-07-07 13:15:29 +0300

    spring api stater created

---
+++ src/main/java/com/itechart/test/altir/receiver/TestSpringBatchReseiverApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.test.altir.receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBatchReseiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBatchReseiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itechart/test/altir/receiver/TestSpringBatchReseiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.test.altir.receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBatchReseiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
